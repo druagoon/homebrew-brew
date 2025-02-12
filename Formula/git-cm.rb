496d994bab62c734c39f429bb83d2b35c16139e5
lazyboy <lazyboy.fan@gmail.com>
2025-02-12 16:53:03 +0800

    feat: add git-cm formula

---
+++ Formula/git-cm.rb
@@ -0,0 +1,18 @@
+class GitCm < Formula
+  desc "CLI for creating conventional-commits friendly commit messages"
+  homepage "https://github.com/mainrs/git-cm"
+  url "https://github.com/mainrs/git-cm/archive/refs/tags/v0.2.3.tar.gz"
+  sha256 "1a8960058f7eaff99098e80d9f91017515ed1c2d5f2c337f756a553ffc4a4ff9"
+  license any_of: ["Apache-2.0", "MIT"]
+
+  depends_on "rust" => :build
+  uses_from_macos "zlib"
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "#{bin}/git-cm", "help"
+  end
+end
