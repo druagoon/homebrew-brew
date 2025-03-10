34bf6e2931771d3af1a82e015370d74e43fcd26d
lazyboy <lazyboy.fan@gmail.com>
2025-03-10 19:08:02 +0800

    fix(icli): update description

--- Formula/icli.rb
+++ Formula/icli.rb
@@ -1,5 +1,5 @@
 class Icli < Formula
-  desc "Includes various commonly used utilities for personal use"
+  desc "Personal command-line tool includes various commonly used utilities"
   homepage "https://github.com/druagoon/icli-rs"
   url "https://github.com/druagoon/icli-rs/archive/refs/tags/v1.2.0.tar.gz"
   sha256 "5701f7869d5fed3e4171482db21bb5836bd94b3622a37b532957581c674d33fd"
