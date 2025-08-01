ce14b5c8d1ff1da89be272bba0b8649583a3d6c4
lazyboy <lazyboy.fan@gmail.com>
2025-08-01 17:10:03 +0800

    fix(rotz): add URL and SHA256 to avoid lint error when running brew CI

--- Formula/rotz.rb
+++ Formula/rotz.rb
@@ -1,21 +1,24 @@
 class Rotz < Formula
   desc "Fully cross platform dotfile manager and dev environment bootstrapper"
   homepage "https://github.com/volllly/rotz"
-  version "1.2.1"
+  url "https://github.com/volllly/rotz/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "61bae45578e988c627a8db36fcc934d8d21e30735f576c77312ba7dd66fd99bb"
+  license "MIT"
+  head "https://github.com/volllly/rotz.git", branch: "main"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/volllly/rotz/releases/download/v#{version}/rotz-aarch64-apple-darwin.zip"
+      url "https://github.com/volllly/rotz/releases/download/v1.2.1/rotz-aarch64-apple-darwin.zip"
       sha256 "8d8fccf7bc376613c22c682a6f16944385f80037a3b0d66f99871b676285444c"
     elsif Hardware::CPU.intel?
-      url "https://github.com/volllly/rotz/releases/download/v#{version}/rotz-x86_64-apple-darwin.zip"
+      url "https://github.com/volllly/rotz/releases/download/v1.2.1/rotz-x86_64-apple-darwin.zip"
       sha256 "6283dd4e90b9ad77b1c1e04eba51b72d7e992a33f043b665ab0f28763ec28531"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/volllly/rotz/releases/download/v#{version}/rotz-x86_64-unknown-linux-musl.zip"
+      url "https://github.com/volllly/rotz/releases/download/v1.2.1/rotz-x86_64-unknown-linux-musl.zip"
       sha256 "ff11ac59319cc0e54e388e81afb3b86a1ee7b887e651c2029bc417ba418aad02"
     end
   end
